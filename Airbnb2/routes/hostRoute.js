64c897376b9ab245bb4a4ce919cad22e0f8d37c7
dhruv1228 <dhruvpachauri28@gmail.com>
2026-02-09 22:33:22 +0530

    Airbnb2_2

--- Airbnb2/routes/hostRoute.js
+++ Airbnb2/routes/hostRoute.js
@@ -10,7 +10,7 @@ hostRoute.get('/host/add-home',(req,res,next)=>{
 const registeredHomes=[];
 hostRoute.post('/host/add-home',(req,res,next)=>{
   console.log("Home registered sucessfully",req.body);
-  registeredHomes.push({homename : req.body.homename,amount: req.body.amount});
+  registeredHomes.push(req.body);
   res.render('homeAdded',{pagetitle: 'Home Added'});
   
 });
